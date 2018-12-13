2dda5ab3e4d4436ca2411350a1e49fa9bcb79f39
Leonrdo Gonçalves <ldsz@gft.com>
2018-12-13 10:11:48 -0200

    Find comments using regex

--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strings"
+	"regexp"
 )
 
 func main() {
@@ -18,35 +17,21 @@ func main() {
 	}
 	defer response.Body.Close()
 
-	// Get the response body as a string
-	dataInBytes, err := ioutil.ReadAll(response.Body)
-	pageContent := string(dataInBytes)
-
-	// Find a substr
-	titleStartIndex := strings.Index(pageContent, "<title>")
-	if titleStartIndex == -1 {
-		fmt.Println("No title element found")
-		os.Exit(0)
+	// Read response data in to memory
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("Error reading HTTP body.", err)
 	}
 
-	// The start index of the title is the index of the first
-	// character, the < symbol. We don't want to include
-	// <title> as part of the final value, so let's offset
-	// the index by the number of characters in <title>
-	titleStartIndex += 7
-
-	// Find the index of the closing tag
-	titleEndIndex := strings.Index(pageContent, "</title>")
-	if titleEndIndex == -1 {
-		fmt.Println("No closing tag for title found.")
-		os.Exit(0)
+	// Create a regular expression to find comments
+	re := regexp.MustCompile("<!--(.|\n)*?-->")
+	comments := re.FindAllString(string(body), -1)
+	if comments == nil {
+		fmt.Println("No matches.")
+	} else {
+		for _, comment := range comments {
+			fmt.Println(comment)
+		}
 	}
 
-	// (Optional)
-	// Copy the substring in to a separate variable so the
-	// variables with the full document data can be garbage collected
-	pageTitle := []byte(pageContent[titleStartIndex:titleEndIndex])
-
-	// Print out the result
-	fmt.Printf("Page title %s\n", pageTitle)
 }
